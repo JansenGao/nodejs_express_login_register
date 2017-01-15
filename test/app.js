4ae403d1c8d3a8529d044d16e85e4797f368471b
JansenGao <298066717@qq.com>
2017-01-16 01:18:14 +0800

    login+logout

--- test/app.js
+++ test/app.js
@@ -52,11 +52,11 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);  // 即为为路径 / 设置路由
-app.use('/users', users); // 即为为路径 /users 设置路由
-app.use('/login',routes); // 即为为路径 /login 设置路由
-app.use('/register',routes); // 即为为路径 /register 设置路由
-app.use('/home',routes); // 即为为路径 /home 设置路由
-app.use("/logout",routes); // 即为为路径 /logout 设置路由
+//app.use('/users', users); // 即为为路径 /users 设置路由
+//app.use('/login',routes); // 即为为路径 /login 设置路由
+//app.use('/register',routes); // 即为为路径 /register 设置路由
+//app.use('/home',routes); // 即为为路径 /home 设置路由
+//app.use("/logout",routes); // 即为为路径 /logout 设置路由
 
 
 // catch 404 and forward to error handler
